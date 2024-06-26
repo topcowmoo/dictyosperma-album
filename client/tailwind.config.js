0da45b7ecdda5ee22b8f48cd3292defd691825d8
Salvatore <mammoliti.sam@gmail.com>
2024-06-26 17:24:15 -0400

    added darkmode class, font family, light and dark mode colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#C2CCFF",
+        // Add dark mode colors
+        darkPrimary: "#4A4E69",
+        darkBackground: "#22223B",
+        darkText: "#F2E9E4",
+      },
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
