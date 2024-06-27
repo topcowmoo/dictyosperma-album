c680dd1cacca40d8f0beacc41d71ed98aa4ae40f
Salvatore <mammoliti.sam@gmail.com>
2024-06-27 16:14:07 -0400

    refactored to add header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,37 @@
-import './index.css';
-import Sidebar from './components/Sidebar';
+import "./index.css";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Header
+        sidebarVisible={sidebarVisible}
+        setSidebarOpen={setSidebarVisible}
+        darkMode={darkMode}
+        isLoggedIn={isLoggedIn}
+      />
+      <Sidebar
+        sidebarVisible={sidebarVisible}
+        setSidebarVisible={setSidebarVisible}
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+        isLoggedIn={isLoggedIn}
+      />
+      <div className={`flex-1 ${sidebarVisible ? 'lg:pl-64' : ''}`}>
+        <main className="py-10">
+          <div className="px-4 sm:px-6 lg:px-8">{/* Content */}</div>
+        </main>
+      </div>
     </div>
   );
 }
